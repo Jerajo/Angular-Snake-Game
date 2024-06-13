bf323d4fb7546aeddf11ba89c624f1f5dc4965f5
jesse.jose <JJoseDelaRosa.Simtlix@supervielle.com.ar>
2024-06-13 07:05:31 -0300

    pruebas actualizadas

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,37 @@
+var preset = require("jest-preset-angular/jest-preset");
+const esModules = ["@angular"].join("|");
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
+  ...preset,
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  verbose: true,
+  testMatch: ["**/*.spec.ts"],
+  moduleDirectories: ["node_modules", "."],
+  roots: ["<rootDir>/src"],
+  globals: {
+    ...preset.globals,
+    "ts-jest": {
+      ...preset.globals["ts-jest"],
+      tsconfig: "tsconfig.spec.json",
+      isolatedModules: true,
+    },
+  },
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test_results",
+        outputName: "obi-frontend-test-result.xml",
+      },
+    ],
+  ],
+  resolver: "jest-preset-angular/build/resolvers/ng-jest-resolver.js",
+  transformIgnorePatterns: [`node_modules/(?!${esModules})`],
+  transform: {
+    "^.+\\.(ts|js|mjs|html|svg)$": "jest-preset-angular",
+  },
+  setupFiles: ["jest-canvas-mock"],
+  testRunner: "jest-jasmine2",
+  // globalSetup: "jest-preset-angular/global-setup",
 };
